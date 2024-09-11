be0b558ee4c230e9d4c1f11e3e79bc115f7d3b1e
Rob <tester@example.com>
2024-09-11 20:11:14 +1000

    Adding Restaurant class

---
+++ src/main/java/com/example/dinnerreserver/model/Restaurant.java
@@ -0,0 +1,46 @@
+package com.example.dinnerreserver.model;
+
+public class Restaurant {
+    private int id;
+    private String name;
+    private String description;
+    private String address;
+
+    public Restaurant(String name, String description, String address) {
+        this.name = name;
+        this.description = description;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
