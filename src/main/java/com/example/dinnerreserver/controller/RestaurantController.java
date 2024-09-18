543bc9c0646b5c3f80cd2b863e7ef3227c9b1ef1
Rob <tester@example.com>
2024-09-18 19:08:50 +1000

    Added Stars that display based on restaurant rating

--- src/main/java/com/example/dinnerreserver/controller/RestaurantController.java
+++ src/main/java/com/example/dinnerreserver/controller/RestaurantController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -26,6 +27,17 @@ public class RestaurantController {
     @FXML
     private Text rating;
 
+    @FXML
+    private ImageView star1;
+    @FXML
+    private ImageView star2;
+    @FXML
+    private ImageView star3;
+    @FXML
+    private ImageView star4;
+    @FXML
+    private ImageView star5;
+
     public RestaurantController() {
         restaurantDAO = new SqliteRestaurantDAO();
         restaurantDAO.insertSampleData();
@@ -35,10 +47,34 @@ public class RestaurantController {
     public void selectRestaurant(Integer restaurantId)
     {
         Restaurant restaurant = restaurantDAO.getRestaurant(restaurantId);
+        Float restaurantRating = restaurant.getRating();
+        String rating_score = STR."\{restaurantRating.toString()} / 5";
         name.setText(restaurant.getName());
         address.setText(restaurant.getAddress());
         description.setText(restaurant.getDescription());
-        rating.setText(restaurant.getRating().toString());
+        rating.setText(rating_score);
+
+        if(restaurantRating > 4.4) {
+            star1.setVisible(true);
+            star2.setVisible(true);
+            star3.setVisible(true);
+            star4.setVisible(true);
+            star5.setVisible(true);
+        } else if(restaurantRating > 3.4) {
+            star1.setVisible(true);
+            star2.setVisible(true);
+            star3.setVisible(true);
+            star4.setVisible(true);
+        } else if(restaurantRating > 2.4) {
+            star1.setVisible(true);
+            star2.setVisible(true);
+            star3.setVisible(true);
+        } else if(restaurantRating > 1.4) {
+            star1.setVisible(true);
+            star2.setVisible(true);
+        } else if(restaurantRating > 0.4) {
+            star1.setVisible(true);
+        }
     }
 
     @FXML
