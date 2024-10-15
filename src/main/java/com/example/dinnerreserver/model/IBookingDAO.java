41126a0e3e7eb924657f8baa13beaf9b95a5c9eb
Rob <tester@example.com>
2024-10-15 23:21:29 +1000

    Added Javadoc comments to Interface DAOs

--- src/main/java/com/example/dinnerreserver/model/IBookingDAO.java
+++ src/main/java/com/example/dinnerreserver/model/IBookingDAO.java
@@ -2,17 +2,49 @@ package com.example.dinnerreserver.model;
 
 import java.util.List;
 
+/**
+ * Interface for the Bookings Data Access Object that handles
+ * the CRUD operations for the User class with the database
+ */
 public interface IBookingDAO {
+
+    /**
+     * Adds a new booking to the database
+     * @param booking The booking to add
+     */
     public void addBooking(Booking booking);
 
+    /**
+     * Counts the number of bookings in a timeslot for a restaurant
+     * @param restaurantId The restaurant to check
+     * @param timeSlot The timeslot to check
+     * @return
+     */
     public int countBookingsForTimeSlot(int restaurantId, String timeSlot);
 
+    /**
+     * Deletes a booking from the database
+     * @param booking The booking to delete
+     */
     public void deleteBooking(Booking booking);
 
+    /**
+     * Updates an existing booking in the database
+     * @param booking The booking to update
+     */
     public void updateBooking(Booking booking);
 
+    /**
+     * Retrieves a booking from the database
+     * @param id The id of the booking to retrieve
+     * @return The booking with the given id, or null if not found
+     */
     public Booking getBooking(int id);
 
+    /**
+     * Retrieves all bookings from the database
+     * @return A list of all bookings in the database
+     */
     public List<Booking> getAllBookings();
 
 }
--- src/main/java/com/example/dinnerreserver/model/IRestaurantDAO.java
+++ src/main/java/com/example/dinnerreserver/model/IRestaurantDAO.java
@@ -2,16 +2,41 @@ package com.example.dinnerreserver.model;
 
 import java.util.List;
 
+/**
+ * Interface for the Restaurant Data Access Object that handles
+ * the CRUD operations for the User class with the database
+ */
 public interface IRestaurantDAO {
 
+    /**
+     * Adds a new restaurant to the database
+     * @param restaurant The restaurant to add
+     */
     public void addRestaurant(Restaurant restaurant);
 
+    /**
+     * Deletes a restaurant from the database
+     * @param restaurant The restaurant to delete
+     */
     public void deleteRestaurant(Restaurant restaurant);
 
+    /**
+     * Updates an existing restaurant in the database
+     * @param restaurant The restaurant to update
+     */
     public void updateRestaurant(Restaurant restaurant);
 
+    /**
+     * Retrieves a restaurant from the database
+     * @param id The id of the restaurant to retrieve
+     * @return The restaurant with the given id, or null if not found
+     */
     public Restaurant getRestaurant(int id);
 
+    /**
+     * Retrieves all restaurants from the database
+     * @return A list of all restaurants in the database
+     */
     public List<Restaurant> getAllRestaurants();
 
 }
