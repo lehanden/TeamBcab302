8dfa62de8dbcbcecd9efa6c6da7b303cc9063f3b
Rob <tester@example.com>
2024-10-15 21:36:38 +1000

    Added Javadoc comments to HelloApplication

--- src/main/java/com/example/dinnerreserver/HelloApplication.java
+++ src/main/java/com/example/dinnerreserver/HelloApplication.java
@@ -7,13 +7,24 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * A simple class to launch the application
+ */
 public class HelloApplication extends Application {
 
+    /**
+     * The application title
+     */
     // Constants defining the window title and size
     public static final String TITLE = "SB Eats";
     //public static final int WIDTH = 1000;
     //public static final int HEIGHT = 664;
 
+    /**
+     * Loads the landing page
+     * @param stage
+     * @throws IOException
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("landingpage.fxml"));
@@ -27,6 +38,10 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    /**
+     * Start the application
+     * @param args
+     */
     public static void main(String[] args) {
         launch();
     }
