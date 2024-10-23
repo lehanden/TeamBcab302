29bcffca42f836b26562b2322f4e279cc7503bbf
Darcy Henman <darcyhenman@gmail.com>
2024-10-24 01:58:44 +1000

    valid and duplicate username/email tests now passing, put into main branch

--- src/main/java/com/example/dinnerreserver/model/User.java
+++ src/main/java/com/example/dinnerreserver/model/User.java
@@ -68,6 +68,11 @@ public class User {
         this.email = email;
     }
 
+    public static boolean isValidEmail(String email) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+        return email.matches(emailRegex);
+    }
+
     /**
      * Gets the password of the current user
      * @return The current user's password
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,4 +1,6 @@
+import com.example.dinnerreserver.model.MockUserDAO;
 import com.example.dinnerreserver.model.User;
+import com.example.dinnerreserver.controller.MainController;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,11 +11,13 @@ public class UserTest {
     public void setup(){
         user = new User("john","john.doe@example.com", "password123");
     }
+
     @Test
     public void testGetUserID(){
         user.setId(1);
         assertEquals(1,user.getId());
     }
+
     @Test
     public void testGetUsername(){
         assertEquals("john", user.getUsername());
@@ -24,6 +28,17 @@ public class UserTest {
         assertEquals("jane", user.getUsername());
     }
     @Test
+    public void testDuplicateUsername() {
+        MockUserDAO userDAO = new MockUserDAO();
+        User firstUser = new User("john", "john.doe@example.com", "password123");
+        userDAO.addUser(firstUser);
+        User duplicateUser = new User("john", "jane.doe@example.com", "password321");
+        assertNotNull(userDAO.getUserByUsername("john"));
+        User existingUser = userDAO.getUserByUsername(duplicateUser.getUsername());
+        assertNotNull(existingUser, "Username already exists!");
+    }
+
+    @Test
     public void testGetEmail(){
         assertEquals("john.doe@example.com", user.getEmail());
     }
@@ -32,6 +47,30 @@ public class UserTest {
         user.setEmail("jane.smith@example.com");
         assertEquals("jane.smith@example.com", user.getEmail());
     }
+    @Test
+    public void testValidEmail() {
+        assertTrue(User.isValidEmail("john.doe@example.com"));
+        assertTrue(User.isValidEmail("jane_doe123@example.co.uk"));
+        assertTrue(User.isValidEmail("user+mailbox@domain.com"));
+    }
+    @Test
+    public void testInvalidEmail() {
+        assertFalse(User.isValidEmail("john.doeexample.com"));
+        assertFalse(User.isValidEmail("john.doe@.com"));
+        assertFalse(User.isValidEmail("john.doe@example"));
+        assertFalse(User.isValidEmail("john"));
+    }
+    @Test
+    public void testDuplicateEmail() {
+        MockUserDAO userDAO = new MockUserDAO();
+        User firstUser = new User("john", "john.doe@example.com", "password123");
+        userDAO.addUser(firstUser);
+        User duplicateUser = new User("jane", "john.doe@example.com", "password321");
+        assertNotNull(userDAO.getUserByEmail("john.doe@example.com"));
+        User existingUser = userDAO.getUserByEmail(duplicateUser.getEmail());
+        assertNotNull(existingUser, "Email already in use!");
+    }
+
     @Test
     public void testGetPassword(){
         assertEquals("password123", user.getPassword());
